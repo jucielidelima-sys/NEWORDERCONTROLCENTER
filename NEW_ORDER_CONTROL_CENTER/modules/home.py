6229887493423ac34b50a32409c4d88dea737c0e
jucielidelima-sys <jucielidelima@gmail.com>
2026-04-07 15:28:36 -0300

    Delete NEW_ORDER_CONTROL_CENTER directory

--- NEW_ORDER_CONTROL_CENTER/modules/home.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def render():
-    st.title("NEW ORDER CONTROL CENTER")
-    st.write("Plataforma Industrial Integrada")
--- NEW_ORDER_CONTROL_CENTER/modules/suprimentos.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-def render():
-    st.title("Suprimentos")
